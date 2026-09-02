9689c80b08b1207ee0c666c4596ccc966db2b08f
Yogita Patil <149256572+YogitaBhikajiPtil@users.noreply.github.com>
2026-09-02 16:48:23 +0530

    Update app.js

--- Expense Tracker - Node.js Project/app.js
+++ Expense Tracker - Node.js Project/app.js
@@ -30,8 +30,10 @@ app.get("/", (req, res) => {
 sequelize.sync()
   .then(() => {
     console.log("Tables Created");
-    app.listen(3000, () => {
-      console.log("Server running on port 3000");
-    });
+   const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   })
   .catch(err => console.log(err));
